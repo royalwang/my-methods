bd2c62896909df574d4a558d07bf202e5355e47a
nieyutang <13920365339@163.com>
2018-10-11 09:42:05 +0800

    2018/10/11

---
+++ 软件相关/vscode插件/插件.js
@@ -0,0 +1,98 @@
+/* 
+    Auto Close Tag
+        自动添加HTML / XML关闭标签（ 必备）
+*/
+
+/* 
+    Auto Rename Tag
+        自动重命名配对的HTML / XML标签(必备)
+*/
+
+/* 
+    Beautify
+        格式化javascript， JSON， CSS， Sass， 和HTML
+        Alt + shift + F
+*/
+
+/*
+    Class autocomplete for HTML
+        智能提示HTML class = “”属性（ 必备）
+ */
+
+/* 
+   Code Runner
+       非常强大的一款插件， 能够运行多种语言的代码片段或代码文件： C， C++，Java， JavaScript， PHP， Python， Perl， Ruby， Go等等， 安装完成后， 右上角出现：
+*/
+
+/* 
+   Document This
+       添加注释块
+       快捷键： 按两次Ctrl + alt + d
+*/
+
+/* 
+    HTML CSS Support
+        在 html 标签上写class 智能提示当前项目所支持的样式（ 必备）
+*/
+
+/* 
+    HTML Snippets
+        html 代码片段（ 必备）
+*/
+
+/* 
+    CSS Peek
+        在HTML里面鼠标移到类名上按住ctrl + 单击， 会直接跳转到这个类名设置的样式文件及样式位置
+*/
+
+/* 
+    JavaScript(ES6) code snippets
+        es6代码片段（ 必备）
+*/
+
+/* 
+    JavaScript Snippet Pack
+        js代码片段（ 必备）
+*/
+
+/* 
+    open in browser
+        当前的 html 文件用浏览器打开
+        快捷键alt + b
+*/
+
+/* 
+    Path Intellisense
+        路径自动补全（ 必备）
+*/
+
+/* 
+    vscode-icons
+        目录树图标
+*/
+
+
+/* 
+    vetur
+        语法高亮、 智能感知
+*/
+
+/* 
+    VueHelper
+        vue代码片段
+*/
+
+/* 
+    Vue 2 Snippets
+        vue 2 代码片段
+*/
+
+/* 
+    Minify
+        用于压缩合并 JavaScript 和 CSS 文件
+*/
+
+/* 
+    Image Preview
+        鼠标移到路径里显示图像预览
+*/
